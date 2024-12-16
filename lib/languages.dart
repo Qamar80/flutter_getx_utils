6c191c462c49d3ac2cd08ec4a00a37530eed2233
Qamar Abbas <123517172+Qamar80@users.noreply.github.com>
2024-12-16 13:05:16 -0800

    add gitignore file

--- lib/languages.dart
+++ lib/languages.dart
@@ -2,6 +2,8 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 class Languages extends Translations{
 
+
+
   @override
   Map<String, Map<String, String>> get keys => {
     'en_US':{
